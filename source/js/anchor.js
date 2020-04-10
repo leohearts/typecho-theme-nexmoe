ff2350f2199975989ea47a87cb2abb9ee1079fb3
leohearts <leohearts@ytoworld.tk>
2020-04-10 22:13:23 +0800

    fix anchor

--- source/js/anchor.js
+++ source/js/anchor.js
@@ -1,14 +1,14 @@
 <script>
     leo = document.getElementsByClassName("nexmoe-primary")[0].querySelectorAll("h2");
     for (var i = 0;i<leo.length;i++){
-        leo[i].id = leo[i].firstChild.textContent;
+        leo[i].id = encodeURIComponent(leo[i].firstChild.textContent);
     }
     leo = document.getElementsByClassName("nexmoe-primary")[0].querySelectorAll("h3");
     for (var i = 0;i<leo.length;i++){
-        leo[i].id = leo[i].firstChild.textContent;
+        leo[i].id = encodeURIComponent(leo[i].firstChild.textContent);
     }
     leo = document.getElementsByClassName("nexmoe-primary")[0].querySelectorAll("h1");
     for (var i = 0;i<leo.length;i++){
-        leo[i].id = leo[i].firstChild.textContent;
+        leo[i].id = encodeURIComponent(leo[i].firstChild.textContent);
     }
 </script>
