aff5b844dacfe58e4ab3c2c21f5554012bdea3e9
leohearts <leohearts@ytoworld.tk>
2020-04-11 12:50:17 +0800

    fix linkinnewtab

--- layout/_partial/after-footer.php
+++ layout/_partial/after-footer.php
@@ -1,7 +1,10 @@
 <script src="<?php echo $this->options->jquery_js ?>"></script>
 <script src="<?php echo $this->options->mdui_js ?>"></script>
-<?php if (!empty($this->options->function) && in_array('SmoothScroll', $this->options->function)): ?>
-<script src="<?php echo $this->options->SmoothScroll_js ?>"></script>
+<?php if (!empty($this->options->function) && in_array('SmoothScroll', $this->options->function)) : ?>
+    <script src="<?php echo $this->options->SmoothScroll_js ?>"></script>
 <?php endif; ?>
 <script src="<?php $this->options->themeUrl('source/js/app.js'); ?>"></script>
 <script src="<?php echo $this->options->lazysizes_js ?>"></script>
+<?php if (!empty($this->options->function) && in_array('LinkinNewtab', $this->options->function)) {
+    echo '<script>(function(){var res = document.querySelectorAll("a");for (var i=0;i<res.length;i++){  if(res[i].href.search(document.querySelectorAll("a")[2].href)){    res[i].target="_blank";  }}})();</script>';
+} ?>
--- layout/_partial/head.php
+++ layout/_partial/head.php
@@ -19,7 +19,4 @@
         <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
         <!-- require MetingJS -->
         <script src="https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js"></script>
-        <?php if (!empty($this->options->function) && in_array('LinkinNewtab', $this->options->function)){
-            echo '<script>(function(){var res = document.querySelectorAll("a");for (var i=0;i<res.length;i++){  if(res[i].href.search(document.querySelectorAll("a")[2].href)){    res[i].target="_blank";  }}})();</script>';
-        } ?>
     </head>
