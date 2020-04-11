dee15b784e9f69b7fc2dc617e3b50fd47cb84d66
leohearts <leohearts@ytoworld.tk>
2020-04-11 17:02:47 +0800

    小改动

--- layout/_partial/after-footer.php
+++ layout/_partial/after-footer.php
@@ -6,5 +6,5 @@
 <script src="<?php $this->options->themeUrl('source/js/app.js'); ?>"></script>
 <script src="<?php echo $this->options->lazysizes_js ?>"></script>
 <?php if (!empty($this->options->function) && in_array('LinkinNewtab', $this->options->function)) {
-    echo '<script>(function(){var res = document.querySelectorAll("a");for (var i=0;i<res.length;i++){  if(res[i].href.search(document.querySelectorAll("a")[2].href)){    res[i].target="_blank";  }}})();</script>';
+    echo '<script>(function(){var res = document.querySelectorAll("a");for (var i=0;i<res.length;i++){  if(res[i].href!=""&&res[i].href.search(document.querySelectorAll("a")[2].href)){    res[i].target="_blank";  }}})();</script>';
 } ?>
